0370fd0b9f8d5f328df2352ba1d795d43eb9b312
Geoffrey Ouvrard <geoffrey.1796@gmail.com>
2021-08-31 00:49:23 +0200

    tests des deux derniers templates ajoutés

--- src/main/java/com/PayMyBuddy/MoneyTransfer/config/WebConfig.java
+++ src/main/java/com/PayMyBuddy/MoneyTransfer/config/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry)
     {
+        registry.addViewController("").setViewName("index");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         //registry.addViewController("/403").setViewName("403");
--- src/test/java/com/PayMyBuddy/MoneyTransfer/controller/UserControllerIT.java
+++ src/test/java/com/PayMyBuddy/MoneyTransfer/controller/UserControllerIT.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static com.PayMyBuddy.MoneyTransfer.util.Json.stringify;
-import static com.PayMyBuddy.MoneyTransfer.util.Json.toJson;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,14 +23,31 @@ public class UserControllerIT {
     @Autowired
     MockMvc mockMvc;
 
+    private ContactDto contactDto = new ContactDto();
+
     @WithMockUser(username = "son.goku@mail.com")
     @Test
     public void testAddContact() throws Exception {
-        ContactDto contactDto = new ContactDto();
+
         contactDto.setEmail("john.doe@mail.com");
         mockMvc.perform(post("/addContact").
                         flashAttr("contact", contactDto))
                 .andExpect(redirectedUrl("/addContact?success"));
     }
 
+    @WithMockUser(username = "son.goku@mail.com")
+    @Test
+    public void testShowNewContactForm() throws Exception{
+        mockMvc.perform(get("/addContact"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("new-contact-form"));
+    }
+
+    @WithMockUser(username = "john.doe@mail.com")
+    @Test
+    public void testShowContactList() throws Exception{
+        mockMvc.perform(get("/contacts"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("contactList", hasItem(hasProperty("email", equalTo("son.goku@mail.com")))));
+    }
 }
